9566298e7a495f6016c56a550f17fb9710a3ff29
JamesSimel <jameskarinosimel@gmail.com>
2023-01-29 14:54:37 +0300

    gRPC way for clarifai API

--- controllers/image.js
+++ controllers/image.js
@@ -1,21 +1,54 @@
-const clarifai = require('clarifai');
+//New Way with gRPC
+const {ClarifaiStub, grpc} = require("clarifai-nodejs-grpc");
+
+const stub = ClarifaiStub.grpc();
+
+const metadata = new grpc.Metadata();
+metadata.set("authorization", "Key a98ffe3acb7b4ed884e7ab5a5811c7b0");
+
+// const clarifai = require('clarifai'); // old way
 
 
-const app = new Clarifai.App({
-    apiKey: 'a98ffe3acb7b4ed884e7ab5a5811c7b0'
-   });
-
+// const app = new Clarifai.App({
+//     apiKey: ''
+//    });
 const hanldeApiCall = (req, res) => {
-    app.models
-    .predict(
+    stub.PostModelOutputs(
         {
-        id: 'face-detection',
-        name: 'face-detection',
-        version: '6dc7e46bc9124c5c8824be4822abe105',
-        type: 'visual-detector',
+            // This is the model ID of a publicly available General model. You may use any other public or custom model ID.
+            model_id: "aaa03c23b3724a16a56b629203edc62c",
+            inputs: [{data: {image: {url: "https://samples.clarifai.com/dog2.jpeg"}}}]
         },
-        req.body.input
-    )
+        metadata,
+        (err, response) => {
+            if (err) {
+                console.log("Error: " + err);
+                return;
+            }
+
+            if (response.status.code !== 10000) {
+                console.log("Received failed status: " + response.status.description + "\n" + response.status.details);
+                return;
+            }
+
+            console.log("Predicted concepts, with confidence values:")
+            for (const c of response.outputs[0].data.concepts) {
+                console.log(c.name + ": " + c.value);
+            }
+        }
+    );
+
+    //Old way of predicting
+    // app.models
+    // .predict(
+    //     {
+    //     id: 'face-detection',
+    //     name: 'face-detection',
+    //     version: '6dc7e46bc9124c5c8824be4822abe105',
+    //     type: 'visual-detector',
+    //     },
+    //     req.body.input
+    // )
 }
    
 const handleImage = (req, res, db) => {
